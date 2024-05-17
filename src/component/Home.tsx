e8c7d90cc9b3bb9d4ee03487b6b405abf8178ac3
dbu <dbu@contraste.com>
2024-05-17 17:21:39 +0100

    init

---
+++ src/component/Home.tsx
@@ -0,0 +1,13 @@
+
+
+const Home =()=>{
+    return(
+        <article>
+            <header>
+                <h1>Byfood Library</h1>
+            </header>
+        </article>
+    )
+}
+
+export default Home
