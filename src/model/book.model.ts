8745d31bb20665e3fb684839a5a9399532e342a4
dbu <dbu@contraste.com>
2024-05-17 21:32:46 +0100

    update book model

--- src/model/book.model.ts
+++ src/model/book.model.ts
@@ -1,23 +1,26 @@
 export default class Book {
-    BookId     ?:    number
+    BookId     :    number
     Title      :     string
     Author      :    string
-    PublicationYear? : pubYear
-    Isbn       ?:     string
+    PublicationYear : pubYear
+    Isbn       :     string
     NumberInShelf :  number
     NumberBorrowed? : number
 
-
-    constructor(BookId: number, Title: string, Author: string,  numberInShelf: number  ){
+    constructor(BookId: number, Title: string, Author: string, publicationYear : pubYear, isbn : string,numberInShelf: number,numberBorrowed : number  ){
         this.BookId=BookId;
         this.Title=Title;
         this.Author=Author;
         this.NumberInShelf=numberInShelf;
+        this.PublicationYear=publicationYear;
+        this.Isbn=isbn;
+        this.NumberBorrowed=numberBorrowed;
     }
+
 
 }
 
-class pubYear {
+export class pubYear {
     Year !: number
     Month !: string
 }
